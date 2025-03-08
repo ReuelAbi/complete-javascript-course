289d7d602a651c5f24479c53ba76e036ff27f9b4
ReuelAbi <mrabishai@gmail.com>
2025-03-08 00:09:23 -0700

    Update-2
    Update-2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 ---
@@ -70,8 +70,8 @@ console.log(ageThree);
     const age = 2037 - birthYear;
     const retirement = 65 - age;
     return retirement;
-} 
-console.log(yearsToRetire(1991));    
+}
+console.log(yearsToRetire(1991));
 */
 
 /* const yearsToRetire = (birthYear, firstName) => {
@@ -80,9 +80,9 @@ console.log(yearsToRetire(1991));
     return `${firstName} retires in ${retirement} years.`;
 }
 console.log(yearsToRetire(1991, 'Jonas'));
-console.log(yearsToRetire(1980, 'Bob')); 
+console.log(yearsToRetire(1980, 'Bob'));
 
-Downside of Arrow functions: cannot use 'this' keyword on them. 
+Downside of Arrow functions: cannot use 'this' keyword on them.
 */
 
 /* function cutFruitPieces(fruit) {
@@ -98,8 +98,8 @@ function processFruit(apples, oranges) {
 }
 console.log(processFruit(2, 3)); */
 
-const friends = ['Mike', 'Steve', 'Peter'];
-console.log(friends);
+// const friends = ['Mike', 'Steve', 'Peter'];
+/* console.log(friends);
 
 const years = new Array(1991, 1984, 2008, 2020);
 console.log(years);
@@ -115,6 +115,147 @@ console.log(friends); // In JS, 'const' can only const Primitive Data Types. Arr
 const jonas = ['Jonas', 'Bob', 2037 - 1991, 'teacher', friends];
 console.log(jonas);
 console.log(jonas.length);
-console.log(typeof (jonas));
+console.log(typeof (jonas)); */
 
-const newLength = friends.push('')
+// Add elements
+/* const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+console.log(friends.pop());
+console.log(friends);
+
+friends.shift(); // first element
+console.log(friends);
+
+console.log(friends.indexOf('Steven')); // Returns -1 for an element that is not found in the array
+console.log(friends.indexOf('Steve'));
+
+friends.push(23);
+console.log(friends.includes('Bob')); // Returns true or false for an element found in the array
+console.log(friends.includes('Steve'));
+console.log(friends.includes(23)); */
+
+/* const jonasArray = [
+    'Jonas',
+    'Schmid',
+    2037 - 1991,
+    'teacher',
+    ["Michael", "Peter", "Steve"]
+]
+
+// Object Literal Syntax in JS
+// Order of values matter in Arrays but not in Objects.
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmid',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ["Michael", "Peter", "Steve"];
+} */
+
+/* const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmid',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ["Michael", "Peter", "Steve"]
+}
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['last' + nameKey]);
+console.log(jonas['first' + nameKey]);
+
+const inputValue = prompt('Enter firstName or lastName');
+console.log(jonas[inputValue]);
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonas';
+console.log(jonas); */
+
+/* const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmid',
+    birthyear: 1991,
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ["Michael", "Peter", "Steve"],
+    hasDriversLicense: true,
+
+     calcAge: function () {
+        // console.log(this);
+        return 2037 - this.birthYear;
+    }
+
+    // a value to the property
+    calcAge: function () {
+        this.age = 2037 - this.birthyear;
+        return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is ${this.calcAge()} years old, and has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+    }
+};
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.getSummary());  */
+
+/* const jonasArray = [
+    'Jonas',
+    'Schmid',
+    2037 - 1991,
+    'teacher',
+    ["Michael", "Peter", "Steve"],
+    true
+]
+const types = []; */
+/*
+for (let i = 0; i < jonasArray.length; i++) {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+}
+
+console.log(types); */
+
+// continue and break
+/* for (let i = 0; i < jonasArray.length; i++) {
+
+    if (typeof jonasArray[i] !== 'string') continue;
+    console.log(jonasArray[i], typeof jonasArray[i]);
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+} */
+
+/* console.log("Only strings")
+for (let i = 0; i < jonasArray.length; i++) {
+
+    if (typeof jonasArray[i] !== 'string') continue;
+    console.log(i);
+    console.log(jonasArray[i], typeof jonasArray[i]);
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+}
+
+console.log("Only break")
+for (let i = 0; i < jonasArray.length; i++) {
+
+    if (typeof jonasArray[i] === 'number') break;
+    console.log(i);
+    console.log(jonasArray[i], typeof jonasArray[i]);
+    // types[i] = typeof jonasArray[i];
+    types.push(typeof jonasArray[i]);
+} */
+
+const x = 23;
+console.log("Hello World!");
